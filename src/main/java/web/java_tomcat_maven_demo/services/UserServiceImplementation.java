06f856c716f888636b64ac8e60095e84e9c77ce8
jtoews32 <jtoews32@gmail.com>
2015-12-04 23:21:42 -0800

    Some testing updates

--- src/main/java/web/java_tomcat_maven_demo/services/UserServiceImplementation.java
+++ src/main/java/web/java_tomcat_maven_demo/services/UserServiceImplementation.java
@@ -1,7 +1,5 @@
 package web.java_tomcat_maven_demo.services;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
--- src/test/java/web/java_tomcat_maven_demo/UserServiceImplementationTest.java
+++ src/test/java/web/java_tomcat_maven_demo/UserServiceImplementationTest.java
@@ -1,17 +1,20 @@
 package web.java_tomcat_maven_demo;
 
-import static org.junit.Assert.*;
-
+// import static org.junit.Assert.*;
+/*
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+*/
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import web.java_tomcat_maven_demo.services.UserService;
 import web.java_tomcat_maven_demo.services.UserServiceImplementation;
 
 public class UserServiceImplementationTest {
 
+	/*
 	@Before
 	public void setUp() throws Exception {
 	}
@@ -22,12 +25,18 @@ public class UserServiceImplementationTest {
 
 	@Test
 	public void test() {
+		
+		System.out.println("This is JUnit Example");
+		
+		
 		ApplicationContext context = new AnnotationConfigApplicationContext( UserServiceImplementation.class);
 		UserService service = context.getBean(UserService.class);
 		service.printMessage();
 		
-		// fail("Not yet implemented");
+		fail("Not yet implemented");
 	}
+	
+	*/
 }
 
 
