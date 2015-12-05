67bd21b66d68b9e2e316e1230c57b79a77abd499
jtoews32 <jtoews32@gmail.com>
2015-12-05 00:16:42 -0800

    Add testgn files

--- src/test/java/web/java_tomcat_maven_demo/UserServiceImplementationTest.java
+++ src/test/java/web/java_tomcat_maven_demo/UserServiceImplementationTest.java
@@ -1,48 +1,19 @@
 package web.java_tomcat_maven_demo;
 
-/*
-import static org.junit.Assert.*;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-*/
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.testng.annotations.DataProvider;
-
 import web.java_tomcat_maven_demo.services.UserService;
 import web.java_tomcat_maven_demo.services.UserServiceImplementation;
 
 public class UserServiceImplementationTest {
-
-	
-	
-	
-	/*
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
 	public void test() {
-		
-		System.out.println("This is JUnit Example");
-		
-		
 		ApplicationContext context = new AnnotationConfigApplicationContext( UserServiceImplementation.class);
 		UserService service = context.getBean(UserService.class);
 		service.printMessage();
-		
-		fail("Not yet implemented");
 	}
-	
-	*/
 }
 
 
